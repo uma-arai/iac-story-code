ab1778673f1d4989210eb2abdd064d891397d249
Masaya ARAI <masaya0708@gmail.com>
2021-06-05 02:10:48 +0900

    Terraform: create Internet Gateway

--- terraform/module/infrastructure/vpc.tf
+++ terraform/module/infrastructure/vpc.tf
@@ -1,5 +1,6 @@
 locals {
   vpc_prefix = "${var.cnis_resource_prefix}-vpc"
+  igw_prefix = "${var.cnis_resource_prefix}-igw"
 }
 
 resource "aws_vpc" "main" {
@@ -8,6 +9,14 @@ resource "aws_vpc" "main" {
   enable_dns_support   = "true"
   tags = {
     "Name"    = "${local.vpc_prefix}-main"
+    "Project" = var.cnis_project_name
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name"    = "${local.igw_prefix}-main"
     "Project" = var.cnis_project_name
   }
 }
