c49f0eae155568a62a85801cdd214e68148facf6
Masaya ARAI <masaya0708@gmail.com>
2021-06-05 02:36:11 +0900

    Terraform: add route table associated with Internet Gateway

--- terraform/module/infrastructure/vpc.tf
+++ terraform/module/infrastructure/vpc.tf
@@ -2,6 +2,7 @@ locals {
   vpc_prefix    = "${var.cnis_resource_prefix}-vpc"
   igw_prefix    = "${var.cnis_resource_prefix}-igw"
   subnet_prefix = "${var.cnis_resource_prefix}-subnet"
+  rt_prefix     = "${var.cnis_resource_prefix}-rt"
 }
 
 resource "aws_vpc" "main" {
@@ -61,3 +62,21 @@ resource "aws_subnet" "private_egress" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    "Name"    = "${local.rt_prefix}-public"
+    "Project" = var.cnis_project_name
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = var.aws_subnet_cidr_public_ingress
+
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_ingress[each.key].id
+}
