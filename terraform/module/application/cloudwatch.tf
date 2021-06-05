16baab71fd87ca0c81ce72ff4feaa5602a1f3ca6
Masaya ARAI <masaya0708@gmail.com>
2021-06-05 12:38:51 +0900

    Terraform: add CloudWatch logs

---
+++ terraform/module/application/cloudwatch.tf
@@ -0,0 +1,11 @@
+locals {
+  logs_prefix = "${var.cnis_resource_prefix}-logs-"
+}
+
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "${local.logs_prefix}-app"
+  retention_in_days = 7
+  tags = {
+    "Project" : var.cnis_project_name
+  }
+}
