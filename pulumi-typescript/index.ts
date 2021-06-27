f19e846832210c25afd09f67a32e8c219f6e7aa9
horsewin <mamamatz@gmail.com>
2021-06-28 02:03:58 +0900

    at once

--- pulumi-typescript/index.ts
+++ pulumi-typescript/index.ts
@@ -1,9 +1,36 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+import { getConfig } from "./configs/config";
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const cfg = getConfig();
+
+const vpcName = `${cfg.resourcePrefix}-vpc-main`;
+const vpc = new awsx.ec2.Vpc(vpcName, {
+  cidrBlock: "10.100.0.0/16",
+  tags: {
+    Name: vpcName
+  },
+  numberOfAvailabilityZones: 2,
+  numberOfNatGateways: 0
+});
+
+const nameWithTag = async () => {
+  const igw = await vpc.internetGateway;
+  if (!igw) {
+    throw new Error("error");
+  }
+
+  new aws.ec2.Tag("igw-tag", {
+    resourceId: igw.internetGateway.id,
+    key: "Name",
+    value: `${cfg.resourcePrefix}-igw-main`
+  });
+};
+
+nameWithTag();
+
+// Export a few resulting fields to make them easy to use:
+export const vpcId = vpc.id;
+export const vpcPrivateSubnetIds = vpc.privateSubnetIds;
+export const vpcPublicSubnetIds = vpc.publicSubnetIds;
